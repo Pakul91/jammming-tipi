c7586868c78ff0760a3b4808f5f2b31ec7ff33e6
Tomasz <pakulniewicz.t@gmail.com>
2021-08-20 09:19:13 +0100

    Jammming 0.0.3

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Track.css";
+
+class Track extends React.Component {
+  renderAction() {
+    return (
+      <button className="Track-action">
+        {this.props.isRemoval ? "-" : "+"}
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>track name </h3>
+          <p>track artist | track album </p>
+          {this.renderAction}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Track;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,9 +5,6 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <p>Kutanga</p>
-        <p>pyton</p>
-        <p>siurek</p>
         {/* <!-- You will add a map method that renders a set of Track components  --> */}
       </div>
     );
