3efc39cf1deed447232a171163529cddb8300dda
Tomasz <pakulniewicz.t@gmail.com>
2021-08-30 15:16:04 +0100

    adding restart func 0.0.1

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -19,49 +19,46 @@ class SearchBar extends React.Component {
     this.setState({ term: e.target.value });
   }
 
-  ConnectOrSearch() {
-    if (this.props.isConnected) {
-      return (
-        <div className="SearchBar">
-          <input
-            placeholder="Enter A Song, Album, or Artist"
-            onChange={this.handleTermChange}
-          />
-          <button
-            className="btn SearchButton"
-            onClick={() => {
-              this.search();
-            }}
-          >
-            SEARCH
-          </button>
+  ConnectBar() {
+    return (
+      <div className="SearchBar">
+        <button className="btn connectButton" onClick={this.props.onConnection}>
+          Click to connect to Spotify!
+        </button>
+        <div className="description">
+          <h3>You haven't connected yet or your access token has expired.</h3>
+          <p>
+            Clicking the above button will connect the application to your
+            Spotify account. This will allow you to search for songs and artists
+            to create your own custom-made playlist and upload it to your
+            Spotify account!
+          </p>
         </div>
-      );
-    } else {
-      return (
-        <div className="SearchBar">
-          <button
-            className="btn connectButton"
-            onClick={this.props.onConnection}
-          >
-            Click to connect to Spotify!
-          </button>
-          <div className="description">
-            <h3>You haven't connected yet or your access token has expired.</h3>
-            <p>
-              Clicking the above button will connect the application to your
-              Spotify account. This will allow you to search for songs and
-              artists to create your own custom-made playlist and upload it to
-              your Spotify account!
-            </p>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
+
+  searchBar() {
+    return (
+      <div className="SearchBar">
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+        />
+        <button
+          className="btn SearchButton"
+          onClick={() => {
+            this.search();
+          }}
+        >
+          SEARCH
+        </button>
+      </div>
+    );
   }
 
   render() {
-    return this.ConnectOrSearch();
+    return this.props.isConnected ? this.searchBar() : this.ConnectBar();
   }
 }
 
